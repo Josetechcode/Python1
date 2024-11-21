212bc726c94585d9aa032989611c504a6cbc6cb9
Josetechcode <chema1987@hotmail.com>
2024-11-21 14:22:50 -0500

    guia1

--- eje9.py
+++ eje9.py
@@ -1,2 +1,23 @@
 # DISPLAY PROGRAM TITLE
-print ("*** PROGRAMA QUE CALCULA NÓMINA DE UNA EMPRESA ***")
+print ("*** PROGRAMA QUE CALCULA LA SUMA DE LOS NÚMEROS PARES Y EL PRODUCTO DE LOS NÚMEROS PARES INGRESADOS ***")
+
+Num = int(input("Cuántos números quieres ingresar?: "))
+
+i = 0
+list_1 = []
+list_2 = []
+while i < Num:
+  i+=1
+  Num1 = int(input(f"Ingresar número: "))
+  
+  if Num1 % 2 == 0:
+    list_1.append(Num1)
+  else:
+    list_2.append(Num1)
+
+print("Los elementos a sumar son ",list_1,".\nLa suma de los numeros es: ",sum(list_1))
+
+product = 1
+for x in list_2:
+  product *= x
+print("Los elementos a multiplicar son: ",list_2,".\nEl producto de los números es: ",product,".")
