6d1df829a4c0fab0b1b5c4578ed1197d370264e5
Josetechcode <chema1987@hotmail.com>
2024-11-21 18:14:18 -0500

    guia1

--- eje13.py
+++ eje13.py
@@ -1,10 +1,15 @@
-# DISPLAY PROGRAM TITLE
-print ("*** PROGRAMA QUE CALCULA DESCUENTO SEGÚN MEMBRESIA ***")
+# DISPLAYING PROGRAM TITLE
+print ("*** PROGRAMA QUE INFORMA EL DESCUENTO SEGÚN LA MEMBRESIA ***")
 
+# PROMPTING THE USER THE MEMBERSHIP
 member = input("Ingresar tipo de membresía: ")
 
+# DISPLAYING THE DISCOUNT AS PER ENTERED MEMBERSHIP
 if member == 'A':
+    print ("Corresponde un descuento en la compra de 10%.")
 
 elif member == 'B':
+    print ("Corresponde un descuento en la compra de 15%.")
 
 elif member == 'C':
+    print ("Corresponde un descuento en la compra de 20%.")
