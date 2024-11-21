ed63e0ca70a33ebb8435b6d4c0b1a83b8a5e1a42
Josetechcode <chema1987@hotmail.com>
2024-11-21 16:00:26 -0500

    guia1

---
+++ eje13.py
@@ -0,0 +1,10 @@
+# DISPLAY PROGRAM TITLE
+print ("*** PROGRAMA QUE CALCULA DESCUENTO SEGÚN MEMBRESIA ***")
+
+member = input("Ingresar tipo de membresía: ")
+
+if member == 'A':
+
+elif member == 'B':
+
+elif member == 'C':
