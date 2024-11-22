0bfb6ecc6950841db97415d6f02ae17d105b823f
Josetechcode <chema1987@hotmail.com>
2024-11-21 22:45:18 -0500

    guia1

--- eje19.py
+++ eje19.py
@@ -1,10 +1,12 @@
 # DISPLAYING PROGRAM TITLE
 print ("*** PROGRAMA QUE CONVIERTE DE GRADOS A RADIANES Y VICEVERSA*** ")
 
+# PROMPT THE USER WHAT CONVERSION THEY WANT TO DO
 Conver = 0
 while Conver != 1 and Conver != 2:
     Conver = int(input("Qué operación quiere realizar?:\n1. Grados a radianes.\n2. Radianes a grados.\n"))
 
+# PROMPT THE USER THE VALUE EN CALCULATING THE NEW VALUE
 if Conver == 1:
     valor = int(input("Ingresa el valor en grados: "))
     print("El valor en radianes es: ",valor * 3.1416/180," rad")
