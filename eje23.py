ea260601ba1f6ce81c34d05ec88aa9ef3a44eb7c
Josetechcode <chema1987@hotmail.com>
2024-11-22 17:19:37 -0500

    guia1

--- eje23.py
+++ eje23.py
@@ -11,6 +11,8 @@ Note_list.append(Nota2)
 Nota3 = int (input("Ingresar la segunda nota [1-100]: "))
 Note_list.append(Nota3)
 
+print(Note_list)
+
 if Nota1 > Nota2 and Nota1 > Nota3:
   print(f"(La nota {Nota1} fue la nota más alta")
   if Nota2 > Nota3:
@@ -26,10 +28,12 @@ elif Nota2 > Nota1 and Nota2 > Nota3:
     print(f"La nota {Nota1} fue la nota más baja")
 
 elif Nota3 > Nota1 and Nota3 > Nota2:
-  print(f"La nota {Nota2} fue la nota más alta")
+  print(f"La nota {Nota3} fue la nota más alta")
   if Nota1 > Nota2:
     print(f"La nota {Nota2} fue la nota más baja")
   else:
     print(f"La nota {Nota1} fue la nota más baja")
+
+
 
 print("La nota 100 fue obtenida por",Note_list.count(100), "estudiantes.")
